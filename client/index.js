e07fee803fea583ee5cc279901eabaebad93a0c8
evansmith-github <esmith40@student.mtsac.edu>
2020-03-14 16:14:15 -0700

    initial commit, project under way

---
+++ client/index.js
@@ -0,0 +1,93 @@
+console.log("new script attached...");
+
+// html element selectors
+const form = document.querySelector(".post-form");
+const postsDiv = document.querySelector(".posts-wrapper");
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const formContent = new FormData(form);
+    const newPost = {
+        name: formContent.get("username"),
+        post: formContent.get("post")
+    }
+    newPostToDB(newPost);    
+});
+
+const postsUrl = "http://localhost:3000/posts";
+const newPostToDB = async (newPost) => {
+    try {
+        await fetch(postsUrl, {
+            method: "POST",
+            body: JSON.stringify(newPost),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        });
+        queryAllPosts()
+    }
+    catch {
+        console.log("error while posting");
+    }
+}
+
+const queryAllPosts = async () => {
+    const posts = await fetch(postsUrl);
+    console.log(posts);
+    const postsArr = await posts.json();
+    console.log(postsArr);
+    displayAllPosts(postsArr);
+}
+
+const displayAllPosts = (postsArr) => {
+    //sort posts by votes, then iterate and display
+    // const rankedPosts = sortByVotes(postsArr);
+    // for(let post of rankedPosts)...
+
+    postsDiv.innerHTML = "";
+
+    for(let post of postsArr) {
+        //create wrapper elements for post elements
+        let newPost = document.createElement("div");
+        let voteDiv = document.createElement("div");
+        let postContent = document.createElement("div");
+
+        //create html elements for username, post, and date
+        let user = document.createElement("h4");
+        user.textContent = post.user;
+
+        let postText = document.createElement("p");
+        postText.textContent = post.post;
+
+        let postDate = document.createElement("h5");
+        postDate.textContent = post.date;
+
+        postContent.appendChild(user);
+        postContent.appendChild(postText);
+        postContent.appendChild(postDate);
+
+        //create html elements for upvotes and downvotes
+        let upVotes = document.createElement("p");
+        upVotes.innerText = post.upVotes;
+
+        let downVotes = document.createElement("p");
+        downVotes.innerText = post.downVotes;
+
+        voteDiv.appendChild(upVotes);
+        voteDiv.appendChild(downVotes);
+
+        newPost.appendChild(postContent);
+        newPost.appendChild(voteDiv);
+
+        //adding relevant css classes for styling
+        voteDiv.classList.add("vote-div");
+        postContent.classList.add("post-content");
+        newPost.classList.add("post-div");
+
+        //append new post div to div wrapper element
+        postsDiv.appendChild(newPost);
+    }
+}
+
+//shows posts when page loads
+queryAllPosts();
---
+++ server/app.js
@@ -0,0 +1,86 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+//db connection
+const connectToDb = async () => {
+    return await mongoose.connect("mongodb://localhost/rankChat", {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
+}
+const db = connectToDb();
+
+//db schema/model definition
+const Schema = mongoose.Schema;
+const postSchema = new Schema({
+    user: String,
+    post: String,
+    date: String,
+    upVotes: Number,
+    downVotes: Number
+});
+const Post = new mongoose.model("Post", postSchema);
+
+//use cors middleware
+//use express middleware
+app.use(cors());
+app.use(express.json());
+
+
+app.listen(3000, () => {
+    console.log("server is listening...");
+});
+
+app.get("/", (req, res) => {
+    res.send("hit the root route bro");
+});
+
+app.get("/posts", async (req, res) => {
+    const postsArr = await Post.find();
+    res.send(postsArr);
+});
+
+//receive new post and send to db function
+app.post("/posts", (req, res) => {
+    if(postIsValid(req.body)){
+        postToDB(req.body)
+        .then(doc => res.json(doc))
+        .catch(err => console.log(err));
+    } else {
+        res.status(422);
+        res.json({ message: "not a valid post"});
+    }
+});
+
+//validates post before sending to db
+const postIsValid = (req) => {
+    return req.name && req.name.toString().trim() !== ""
+            &&
+           req.post && req.post.toString().trim() !== "";
+}
+
+const postToDB = async (newPost) => {
+    const post = new Post({
+        user: newPost.name,
+        post: newPost.post,
+        date: new Date().toDateString(),
+        upVotes: 0,
+        downVotes: 0
+    });
+    const doc = await post.save();
+    return doc;
+}
+
+
+const updateVotes = (id, direction) => {
+    if(direction === "up") {
+    //Post.find( { upVotes: { $eq: id } } )
+
+    } else {
+    //Post.find( { downVotes: { $eq: id } } )
+
+    }
+    console.log("updated!");
+}
