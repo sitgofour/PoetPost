a38487f66ed184fa117623df7dd7d0c7658c392c
sitgofour <evansmithcodes@gmail.com>
2020-05-01 19:12:25 -0700

    port change

--- server/app.js
+++ server/app.js
@@ -27,8 +27,9 @@ const Post = new mongoose.model("Post", postSchema);
 app.use(cors());
 app.use(express.json());
 
+const port = process.env.PORT || 3000
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log("server is listening...");
 });
 
