36b80ee763205171e992000aff6a4c0b1fbec8a1
sitgofour <evansmithcodes@gmail.com>
2020-05-01 20:17:47 -0700

    file structure cont

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const path = require("path");
 const cors = require("cors");
 const mongoose = require("mongoose");
 
+app.use(express.static('public'));
+
 //db connection
 const connectToDb = async () => {
     return await mongoose.connect(process.env.MONGODB_URI, {
@@ -35,7 +37,7 @@ app.listen(port, () => {
 });
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname+'/index.html'));
+    res.sendFile(path.join(__dirname+'/public/index.html'));
 });
 
 app.get("/posts", async (req, res) => {
